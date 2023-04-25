abe96e16a7f6fa20e5d5bc3f47499c5655cdc770
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-04-26 00:26:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,12 @@ from speechbrain.pretrained import EncoderClassifier
 
 # Load the encoder classifier model
 classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", savedir="pretrained_models/spkrec-xvect-voxceleb")
-
+def cosine_similarity(x1, x2):
+    dot_product = torch.sum(x1 * x2, dim=-1)
+    norm_x1 = torch.norm(x1, dim=-1)
+    norm_x2 = torch.norm(x2, dim=-1)
+    cosine_similarity = dot_product / (norm_x1 * norm_x2)
+    return cosine_similarity
 
 def load_audio(file):
     # Load audio file using Torchaudio
@@ -21,18 +26,23 @@ def analyze_audio(file):
 
     # Encode the audio signal using the x-vector model
     embeddings_xvect = classifier.encode_batch(waveform)
-
+    return embeddings_xvect
     # Display the embeddings
-    st.write("The x-vector embeddings are:")
-    st.write(embeddings_xvect)
+#     st.write("The x-vector embeddings are:")
+#     st.write(embeddings_xvect)
 
 # Define Streamlit app
 st.title("Audio Analysis")
-st.write("Analyzes the properties of an audio file using the PyDub module.")
+st.write("Comparision of two audio samples using xvectors.")
 
 # Create audio input component
-audio_file = st.file_uploader("Choose an audio file", type=["mp3", "wav", "flac"])
+audio_file1 = st.file_uploader("Choose 1st audio  file", type=["mp3", "wav", "flac"])
 
 # Analyze audio properties when file is uploaded
-if audio_file is not None:
-    analyze_audio(audio_file)
+if audio_file1 is not None:
+    xvect1=analyze_audio(audio_file1)
+audio_file2=st.file_uploader("Choose 2nd audio  file", type=["mp3", "wav", "flac"])
+if audio_file2 is not None:
+    xvect2=analyze_audio(audio_file2)
+st.write("the similarity of the given two audio files is:")
+st.write(cosine_similarity(xvect1,xvect2))
