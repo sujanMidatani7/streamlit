65e80749743167755d2d28ac5046eef571b7f1ce
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-04-25 23:40:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,39 @@
 import streamlit as st
-
-from pydub import AudioSegment
+import torch
 import torchaudio
 from speechbrain.pretrained import EncoderClassifier
+
+# Load the encoder classifier model
 classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", savedir="pretrained_models/spkrec-xvect-voxceleb")
-def analyze_audio(audio_file):
-    # Load audio file using PyDub
-   signal, fs =torchaudio.load(audio_file.name)
-   embeddings_xvect1 = classifier.encode_batch(signal)
-   st.write("the xvector")
-   st.write(embeddings_xvect1)
+
+@st.cache
+def load_audio(file):
+    # Load audio file using Torchaudio
+    try:
+        waveform, sample_rate = torchaudio.load(file, normalization=True)
+        return waveform, sample_rate
+    except:
+        st.error("Error loading audio file.")
+        return None, None
+
+def analyze_audio(file):
+    waveform, sample_rate = load_audio(file)
+    if waveform is None:
+        return
+
+    # Encode the audio signal using the x-vector model
+    embeddings_xvect = classifier.encode_batch(waveform)
+
+    # Display the embeddings
+    st.write("The x-vector embeddings are:")
+    st.write(embeddings_xvect)
 
 # Define Streamlit app
 st.title("Audio Analysis")
 st.write("Analyzes the properties of an audio file using the PyDub module.")
 
 # Create audio input component
-audio_file = st.file_uploader("Choose an audio file", type=["mp3", "wav"])
+audio_file = st.file_uploader("Choose an audio file", type=["mp3", "wav", "flac"])
 
 # Analyze audio properties when file is uploaded
 if audio_file is not None:
