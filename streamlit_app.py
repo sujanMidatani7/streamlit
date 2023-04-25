754d6b74bdfd5470d811b83491d7d06a3e4f4ec2
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-04-25 21:38:17 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from pydub import AudioSegment
+
+
+def analyze_audio(audio_file):
+    # Load audio file using PyDub
+    audio = AudioSegment.from_file(audio_file.name)
+
+    # Analyze audio properties
+    channels = audio.channels
+    sample_width = audio.sample_width
+    frame_rate = audio.frame_rate
+    duration = audio.duration_seconds
+
+    # Display results
+    st.write("Channels:", channels)
+    st.write("Sample Width:", sample_width)
+    st.write("Frame Rate:", frame_rate)
+    st.write("Duration:", duration)
+
+
+# Define Streamlit app
+st.title("Audio Analysis")
+st.write("Analyzes the properties of an audio file using the PyDub module.")
+
+# Create audio input component
+audio_file = st.file_uploader("Choose an audio file", type=["mp3", "wav"])
+
+# Analyze audio properties when file is uploaded
+if audio_file is not None:
+    analyze_audio(audio_file)
