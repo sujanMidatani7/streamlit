6b872f3082fafad056b27433123a6f59f570923d
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-06-23 23:44:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 from gradio_client import Client
 import streamlit as st
-import requests
 
 def read_pdf(file):
-    # Convert the uploaded file to bytes
-    bytes_data = file.getvalue()
-    # Send a post request to the API with the file as data
-    response = requests.post("https://sujanmidatani-resume-details-extractor.hf.space/predict", data=bytes_data)
-    # Parse the JSON response
-    result = response.json()
+    client = Client("https://sujanmidatani-resume-details-extractor.hf.space/")
+    result = client.predict(
+                    file,
+                    api_name="/predict"
+    )
+        
     return result
 
 st.title("PDF to Text")
