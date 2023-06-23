dd86550241dcab7852fb90c511878aad59174e5b
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-06-24 00:33:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from gradio_client import Client
+import tempfile
+import os
 
 def extract_resume_details(pdf_file):
     client = Client("https://sujanmidatani-resume-details-extractor.hf.space/")
     result = client.predict(pdf_file, api_name="/predict")
     with open(result, "r", encoding='utf-8') as f:
         return f.read()
-        
 
 def main():
     st.title("Resume Details Extractor")
@@ -19,16 +20,25 @@ def main():
         # Perform extraction on button click
         if st.button("Extract"):
             try:
-                # Convert the uploaded file to bytes
-                file_bytes = uploaded_file.read()
+                # Create a temporary directory
+                temp_dir = tempfile.mkdtemp()
+                temp_file_path = os.path.join(temp_dir, "resume.pdf")
 
-                # Call the extraction function
-                extracted_details = extract_resume_details(file_bytes)
+                # Save the uploaded PDF file to the temporary location
+                with open(temp_file_path, "wb") as temp_file:
+                    temp_file.write(uploaded_file.read())
+
+                # Call the extraction function with the file path
+                extracted_details = extract_resume_details(temp_file_path)
 
                 # Display the extracted details
                 st.text(extracted_details)
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
+            finally:
+                # Clean up the temporary directory and file
+                os.remove(temp_file_path)
+                os.rmdir(temp_dir)
 
 if __name__ == "__main__":
     main()
