7d5b44eca9cea96af14c7c5c67c7e87eb5e13a7c
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-06-23 23:27:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,20 @@
 from gradio_client import Client
 import streamlit as st
 import base64
+
 def read_pdf(file):
-	client = Client("https://sujanmidatani-resume-details-extractor.hf.space/")
-	result = client.predict(
-					file,
-					api_name="/predict"
-	)
+    client = Client("https://sujanmidatani-resume-details-extractor.hf.space/")
+    result = client.predict(
+                    file.name,
+                    api_name="/predict"
+    )
         
-	return result
+    return result
+
 st.title("PDF to Text")
 file = st.file_uploader("Upload PDF", type=["pdf"])
 if file is not None:
+    # Encode the pdf file using base64 encoding
     encoded_pdf = base64.b64encode(file.read()).decode('utf-8')
     # Send the encoded pdf file to the API
     text = read_pdf(encoded_pdf)
