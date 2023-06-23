f27a31487a77ecb0db46f2e2bcea005b67091706
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-06-23 23:35:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ if file is not None:
     # Encode the pdf file using base64 encoding
     encoded_pdf = base64.b64encode(file.read()).decode('utf-8')
     # Send the encoded pdf file to the API
-    file = open(encoded_pdf, 'r')
-    text = read_pdf(file)
-    st.write(text)
+    with open(encoded_pdf, 'r') as file:
+        text = read_pdf(file)
+        st.write(text)
+        
