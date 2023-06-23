4b153d8653e2c410b811de4bce9039dd0ed28db9
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-06-24 00:26:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,33 @@
-from gradio_client import Client
 import streamlit as st
-import requests
-import os
-def read_pdf(file):
-    # Convert the uploaded file to bytes
-    # bytes_data = file.getvalue()
-    # Send a post request to the API with the file as data
-    client = Client("https://sujanmidatani-resume-details-extractor.hf.space/")
-    result = client.predict(
-		 os.path.abspath(file.name),
-		api_name="/predict"
-	)
-        
-    return result
+from gradio_client import Client
 
-st.title("PDF to Text")
-file = st.file_uploader("Upload PDF", type=["pdf"])
-if file is not None:
-    text = read_pdf(file)
-    st.write(text)
+def extract_resume_details(pdf_file):
+    client = Client("https://sujanmidatani-resume-details-extractor.hf.space/")
+    result = client.predict(pdf_file, api_name="/predict")
+    with open(result, "r", encoding='utf-8') as f:
+        return f.read()
+
+def main():
+    st.title("Resume Details Extractor")
+    st.write("Upload a PDF file to extract resume details.")
+
+    # File uploader
+    uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
+
+    if uploaded_file is not None:
+        # Perform extraction on button click
+        if st.button("Extract"):
+            try:
+                # Convert the uploaded file to bytes
+                file_bytes = uploaded_file.read()
+
+                # Call the extraction function
+                extracted_details = extract_resume_details(file_bytes)
+
+                # Display the extracted details
+                st.text(extracted_details)
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    main()
