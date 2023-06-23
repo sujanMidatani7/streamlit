5230774cdb34f8da8716940484f1084fcf1cb8fb
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-06-24 01:27:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,10 @@ def main():
                     role = st.text_input('Enter your role:')
                     experience = st.text_input('Enter your experience:')
                     d=st.button("Generate Questions")
-                    time.sleep(2)
+                    while(True):
+                        if role is not None and experience is not None:
+                            break
+                        
                     if d: 
                         q = questions_generator(extracted_details, role, experience)
                         st.write("your questions are :")
